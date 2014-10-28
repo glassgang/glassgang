f5bae613a99ca1ed6475c3581ed8628ceac62a92
Lee McAlilly <leemcalilly@gmail.com>
2014-10-27 20:41:44 -0500

    removing what we don't need'

--- js/lower.js
+++ js/lower.js
@@ -1,21 +1,17 @@
 $(document).ready(function() {
 
-  Foundationify.init();
   $('.video-feature').prepend('<div class="video-background"></div>');
-  if (!Modernizr.touch) {
-    $('.video-background').videobackground({
-        videoSource: [
-          ['https://s3.amazonaws.com/fuzz-website/assets/video/of-12.mp4', 'video/mp4'],
-          ['https://s3.amazonaws.com/fuzz-website/assets/video/of-12.webm', 'video/webm'],
-          ['https://s3.amazonaws.com/fuzz-website/assets/video/of-12.ogv', 'video/ogg']
-        ],
-        loop: true,
-        preload: 'auto',
-        resize: false,
-        poster: '/assets/video-still.jpg',
-        loadedCallback: function() {
-          $(this).videobackground('mute');
-        }
-      });
-  }
+  $('.video-background').videobackground({
+      videoSource: [
+        ['https://s3.amazonaws.com/fuzz-website/assets/video/of-12.mp4', 'video/mp4'],
+        ['https://s3.amazonaws.com/fuzz-website/assets/video/of-12.webm', 'video/webm'],
+        ['https://s3.amazonaws.com/fuzz-website/assets/video/of-12.ogv', 'video/ogg']
+      ],
+      loop: true,
+      preload: 'auto',
+      resize: false,
+      poster: '/assets/video-still.jpg',
+      loadedCallback: function() {
+        $(this).videobackground('mute');
+      }
 });
