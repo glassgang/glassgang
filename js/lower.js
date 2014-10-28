8a4e31466f6f13626c712325da6be2c28ed3d6c5
Lee McAlilly <leemcalilly@gmail.com>
2014-10-27 20:59:36 -0500

    trying fuzz js

--- js/lower.js
+++ js/lower.js
@@ -1,8 +1,15 @@
-$('.video-feature').videoBG({
-  mp4:'https://s3.amazonaws.com/fuzz-website/assets/video/of-12.mp4',
-  ogv:'https://s3.amazonaws.com/fuzz-website/assets/video/of-12.ogv',
-  webm:'https://s3.amazonaws.com/fuzz-website/assets/video/of-12.webm',
-  poster:'tunnel_animation.jpg',
-  loop: true,
-  preload: 'auto'
+$('.video-feature').prepend('<div class="video-background"></div>');
+$('.video-background').videobackground({
+    videoSource: [
+      ['https://s3.amazonaws.com/fuzz-website/assets/video/of-12.mp4', 'video/mp4'],
+      ['https://s3.amazonaws.com/fuzz-website/assets/video/of-12.webm', 'video/webm'],
+      ['https://s3.amazonaws.com/fuzz-website/assets/video/of-12.ogv', 'video/ogg']
+    ],
+    loop: true,
+    preload: 'auto',
+    resize: false,
+    poster: '/assets/video-still.jpg',
+    loadedCallback: function() {
+      $(this).videobackground('mute');
+    }
 });
